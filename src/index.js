f680c22ec32ee8e9117b00d99b89376779d5e5d0
artyuhvladislav <workto22@gmail.com>
2019-10-13 10:00:49 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let arrStr = str.split('');
+	for (let i = 0; i < arrStr.length; i++) {
+		for (let j = 0; j < bracketsConfig.length; j++) {
+			let firstBracket = bracketsConfig[j][0], 
+			secondBracket = bracketsConfig[j][1];
+			if (arrStr[i] === firstBracket && arrStr[i + 1] === secondBracket) {
+				arrStr.splice(i, 2);
+				i = -1;  
+			}
+		}
+	}
+	if (arrStr.length === 0){
+		return true
+	}
+	else {
+		return false
+	}
+
 }
